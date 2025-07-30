0aa5951dd76c17bb59c7ab778227065e164ab644
daniel calliari <dbcalliari@gmail.com>
2025-07-30 02:19:57 -0300

    fix: update import from hono/deno to hono/bun in ip.ts

--- server/src/utils/ip.ts
+++ server/src/utils/ip.ts
@@ -1,5 +1,5 @@
 import type { Context } from "hono";
-import { getConnInfo } from "hono/deno";
+import { getConnInfo } from "hono/bun";
 
 export function getIp(c: Context) {
 	return c.req.header("x-real-ip") || getConnInfo(c).remote.address;
